27e45b9eddbea928108b988eec308050c04cced5
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-10-02 11:11:11 +0700

    add jwt dto

---
+++ src/modules/auth/dto/jwt-user.dto.ts
@@ -0,0 +1,7 @@
+export interface JwtUserDto {
+  id: string;
+  email: string;
+  username: string;
+  first_name?: string;
+  last_name?: string;
+}
--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtUserDto } from '../dto/jwt-user.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: JwtUserDto): Promise<JwtUserDto> {
     return {
       id: payload.id,
       email: payload.email,
